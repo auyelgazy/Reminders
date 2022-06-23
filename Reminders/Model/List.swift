3ae575ae9a63919365eea89bc718269071e27b36
Kuanysh Auyelgazin <107985325+auyelgazy@users.noreply.github.com>
2022-06-23 23:39:58 +0600

    blueprint

---
+++ Reminders/Model/List.swift
@@ -0,0 +1,15 @@
+//
+//  List.swift
+//  Reminders
+//
+//  Created by Kuanysh Auyelgazin on 23.06.2022.
+//
+
+import Foundation
+import UIKit
+
+struct List {
+    
+    let name: String
+    let backgroundColor: UIColor
+}
---
+++ Reminders/Model/Reminder.swift
@@ -0,0 +1,14 @@
+//
+//  Reminder.swift
+//  Reminders
+//
+//  Created by Kuanysh Auyelgazin on 23.06.2022.
+//
+
+import Foundation
+
+struct Reminder {
+    
+    let title: String
+    let isDone: Bool
+}
